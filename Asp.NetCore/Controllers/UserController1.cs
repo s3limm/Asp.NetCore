ad145ab6e6188dd75b5244c07e871cce1520dc23
sn3al <108307150+s3limm@users.noreply.github.com>
2022-10-24 16:05:44 +0300

    learned models sheet

--- Asp.NetCore/Controllers/UserController1.cs
+++ Asp.NetCore/Controllers/UserController1.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.NetCore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore.Controllers
 {
     public class UserController1 : Controller
     {
+        //user class'ından inctance aldık ve böylelıkle models katmanını görmüş olduk.
         public IActionResult GetUser()
         {
+            User user = new User();
+
+            user.İd = 1938;
+            user.Name = "Yavuz";
+            user.SurName = "Emrem";
             return View();
+        }
+
+        //bizim burada yazdıgımız metotların hepsı action olarak gecer oyuzden bunları sayfa ıcerısnde eger gostermek ıstemıyorsak [NonAction] komutunu yazmamız gerekır.
+
+        [NonAction]
+        public void GetFullName(User user)
+        {
+            user.SurName = "Selim";
         }
     }
 }
---
+++ Asp.NetCore/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp.NetCore.Models
+{
+    public class User
+    {
+        public int İd { get; set; }
+        public string Name { get; set; }
+        public string SurName{ get; set; }
+    }
+}
